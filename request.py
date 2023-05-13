32518f512b77c7059a7c5139231d585f6ac3036f
a-d-camp <127770512+a-d-camp@users.noreply.github.com>
2023-05-13 18:23:48 -0500

    Update request.py

--- request.py
+++ request.py
@@ -1,5 +1,6 @@
 from venmo_api import Client
 from dotenv import load_dotenv
+from wonderwords import RandomWord
 import os
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -9,6 +10,10 @@ access_token = os.environ.get('VENMO_ACCESS_TOKEN')
 a_id = os.environ.get('A_FRIEND_ID')
 b_id = os.environ.get('B_FRIEND_ID')
 
+r = RandomWord()
+memo_a = r.word(include_parts_of_speech=["nouns", "adjectives"])
+memo_b = r.word(include_parts_of_speech=["nouns", "adjectives"])
+
 client = Client(access_token=access_token)
-client.payment.request_money(5.85, "spotify", a_id)
-client.payment.request_money(5.85, "spotify", b_id)
+client.payment.request_money(5.85, memo_a, a_id)
+client.payment.request_money(5.85, memo_b, b_id)
