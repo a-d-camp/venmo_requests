c7e4f4f98fb98ab027bd030ecdd4961f995ab622
Andrew Camp <andrewcamp@Andrews-MacBook-Air-2.local>
2023-06-21 22:28:23 -0500

    automation changes

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 from venmo_api import Client
 from dotenv import load_dotenv
-import pandas as pd
 import os
+import pickle
 
 # load environment variables
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -9,16 +9,17 @@ load_dotenv()
 access_token = os.environ.get('VENMO_ACCESS_TOKEN')
 
 # load request info from file
-request_data = pd.read_csv('./request_data.csv')
-request_data = request_data.to_dict(orient='index')
+with open('./request_data.pickle', 'rb') as handle:
+    request_data = pickle.load(handle)
 
 # initialize venmo client
 client = Client(access_token=access_token)
 
 # make requests
 for id in request_data:
-    friend_id = request_data[id]['friend_id']
+    #friend_id = request_data[id]['friend_id']
+    name = request_data[id]['friend'].upper()
+    friend_id = os.environ.get(name)
     amount = request_data[id]['amount']
     memo = request_data[id]['reason']
     client.payment.request_money(amount, memo, friend_id)
-
