eaac2ec0adebbe3fdcf0cb36cc8560e4fec9f121
Andrew Camp <andrewcamp@Andrews-MacBook-Air-2.local>
2023-06-21 21:10:04 -0500

    removed random memos

--- request.py
+++ request.py
@@ -1,19 +1,29 @@
 from venmo_api import Client
 from dotenv import load_dotenv
 from wonderwords import RandomWord
+import pandas as pd
 import os
 
+# load environment variables
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv()
-
 access_token = os.environ.get('VENMO_ACCESS_TOKEN')
-a_id = os.environ.get('A_FRIEND_ID')
-b_id = os.environ.get('B_FRIEND_ID')
 
+# random words for fun memos
 r = RandomWord()
-memo_a = r.word(include_parts_of_speech=["nouns", "adjectives"])
-memo_b = r.word(include_parts_of_speech=["nouns", "adjectives"])
 
+# load request info from file
+request_data = pd.read_csv('./request_data.csv')
+request_data = request_data.to_dict(orient='index')
+
+# initialize venmo client
 client = Client(access_token=access_token)
-client.payment.request_money(5.85, memo_a, a_id)
-client.payment.request_money(5.85, memo_b, b_id)
+
+# make requests
+for id in request_data:
+    friend_id = request_data[id]['friend_id']
+    amount = request_data[id]['amount']
+    memo = request_data[id]['reason']
+    # memo = r.word(include_parts_of_speech=["nouns", "adjectives"])
+    client.payment.request_money(amount, memo, friend_id)
+
--- setup_script.py
+++
@@ -1,7 +0,0 @@
-from venmo_api import Client
-from utils import Venmo
-
-access_token = Client.get_access_token(username='',
-                                       password='')
-venmo = Venmo(access_token)
-userId = venmo.get_user_id_by_username("")
